a9a5c73970a4bea251c76597266de9acb2bdf0f7
billygoddard <william.goddard@bobcats.gcsu.edu>
2024-10-18 15:00:28 -0400

    sending to git

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,11 @@ def about():
 @app.route('/hobbies')
 def hobbies():
     return render_template('hobbies.html')
+# add a route for career.html
+@app.route('/career')
+def career():
+    return render_template('career.html')
+# add a route for statistics.html
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
